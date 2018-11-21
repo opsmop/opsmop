6a3cfdc05851128cdbae49133b8eff4864ed8563
Michael DeHaan <michael@michaeldehaan.net>
2018-11-21 10:20:35 -0500

    Add license headers

--- opsmop/meta/docs/generator.py
+++ opsmop/meta/docs/generator.py
@@ -1,10 +1,23 @@
+# Copyright 2018 Michael DeHaan LLC, <michael@michaeldehaan.net>
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import glob
 import os
 
 from opsmop.meta.docs.exparser import Record
 from opsmop.meta.docs.index import Index
 from opsmop.meta.docs.page import Page
-
 
 class Generator(object):
 
--- opsmop/types/asserts.py
+++ opsmop/types/asserts.py
@@ -1,7 +1,20 @@
+# Copyright 2018 Michael DeHaan LLC, <michael@michaeldehaan.net>
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from opsmop.core.field import Field
 from opsmop.core.fields import Fields
 from opsmop.types.type import Type
-
 
 class Asserts(Type):
 
